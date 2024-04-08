3b2913fc517f35c194205cc762a997920e1ea625
Julius van Voorden <juliusvanvoorden@outlook.com>
2024-04-08 23:46:58 +0200

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  project: {
+    link: "https://github.com/shuding/nextra-docs-template",
+  },
+  chat: {
+    link: "test",
+  },
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  footer: {
+    text: "Nextra Docs Template",
+  },
+  primaryHue: 172,
+};
+
+export default config;
